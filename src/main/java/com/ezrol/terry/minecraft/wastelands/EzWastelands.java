c72f99a2f9bb0a524c6888b77ddcf8fa4b1c79ca
Terrence Ezrol <ezterry3@gmail.com>
2016-01-10 20:55:01 -0500

    bump version 0.1.1

--- src/main/java/com/ezrol/terry/minecraft/wastelands/EzWastelands.java
+++ src/main/java/com/ezrol/terry/minecraft/wastelands/EzWastelands.java
@@ -40,7 +40,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @Mod(modid = EzWastelands.MODID, version = EzWastelands.VERSION, name = EzWastelands.NAME)
 public class EzWastelands {
 	public static final String MODID = "ezwastelands";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.1.1";
 	public static final String NAME = "EzWastelands";
 
 	public static Block wastelandBlock;
